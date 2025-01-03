b829711d2183eb23f8e88fdb6375e03a2d0a8790
Phatisena <naphatism@hotmail.com>
2025-01-03 08:13:28 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -190,8 +190,8 @@ namespace aztec {
     //%group="image"
     //%weight=10
     export function genimg(text:string="",gap:number=4,sec:number=undefined,lay:number=undefined) {
-        if(!(sec)) sec = Math.floor((text.length * Math.sqrt(text.length)) / 100);
-        if(!(lay)) lay = Math.floor((text.length * sec) / Math.sqrt(text.length));
+        if(sec == undefined) sec = Math.floor((text.length * Math.sqrt(text.length)) / 4) + 1;
+        if(lay == undefined) lay = Math.max(Math.floor((text.length * sec) / Math.sqrt(text.length)),4);
         let outputnll: number[][] = azgen(text,sec,lay)
         let outputimg: Image = image.create(outputnll[0].length,outputnll.length)
         let bin = 0
