1c25d99a482f754cd36015a176a37611463cca62
Phatisena <naphatism@hotmail.com>
2025-01-03 17:48:37 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -191,7 +191,7 @@ namespace aztec {
             if (idxsum) { v += (text.charCodeAt(i) * i+1) / subnum}
             else { v += text.charCodeAt(i) / subnum}
         }
-        v = v / renum
+        v = v / (renum ^ subnum)
         return v
     }
 
